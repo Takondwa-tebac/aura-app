0080331d3bf9574328b554e2d12a6063ac728f13
Takondwa-tebac <takondwakapyolah@gmail.com>
2025-01-31 08:39:58 +0200

    update: updated landing page

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -11,16 +11,19 @@ import { images } from "../../constants";
 
 import FormField from "@/components/form-field";
 import Button from "@/components/Button";
+import { Link } from "expo-router";
 
 const Login = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="flex flex-col gap-1  w-full h-full px-4 mt-10">
+        <View className="justify-center  w-full  min-h-[83vh] px-4 my-6">
           <Image
             source={images.logo}
             resizeMode="contain"
@@ -44,7 +47,19 @@ const Login = () => {
             onChange={(text: string) => setForm({ ...form, password: text })}
           />
 
-          <Button text="Login" presser={() => {}} />
+          <Button text="Login" presser={() => {}} isLoading={isSubmitting} />
+        </View>
+
+        <View className="flex-row items-center justify-center pt-5 gap-2">
+          <Text className="text-sm font-pregular text-gray-100">
+            Don't have an account?
+          </Text>
+          <Link
+            href="/register"
+            className="text-blue-600 font-pregular  text-sm"
+          >
+            Sign Up
+          </Link>
         </View>
       </ScrollView>
     </SafeAreaView>
--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,27 +1,77 @@
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
 import { images } from "../../constants";
-import { StatusBar } from "expo-status-bar";
+
+import FormField from "@/components/form-field";
+import Button from "@/components/Button";
+import { Link } from "expo-router";
 
 const Register = () => {
-  return (
-    <>
-      <SafeAreaView className="bg-primary h-full">
-        <ScrollView>
-          <View className="flex flex-col gap-4  w-full h-full px-4">
-            <Image
-              source={images.logo}
-              resizeMode="contain"
-              className="w-[130px] h-[84px]"
-            />
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
 
-            <Text className="text-5xl font-pbold text-white my-5"> Login</Text>
+  const [isRegistering, setIsRegistering] = useState(false);
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="flex flex-col gap-1  w-full h-full px-4 mt-10">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[130px] h-[84px]"
+          />
+
+          <Text className="text-2xl font-pbold text-white mt-2">Sign Up</Text>
+          <FormField
+            type="text"
+            title="Username"
+            placeholder="Enter your username"
+            value={form.username}
+            onChange={(text: string) => setForm({ ...form, username: text })}
+          />
+
+          <FormField
+            title="Email"
+            placeholder="Enter your email"
+            type="email"
+            value={form.email}
+            onChange={(text: string) => setForm({ ...form, email: text })}
+          />
+
+          <FormField
+            title="Password"
+            placeholder="Enter your password"
+            type="password"
+            value={form.password}
+            onChange={(text: string) => setForm({ ...form, password: text })}
+          />
+
+          <Button text="Sign Up" presser={() => {}} isLoading={isRegistering} />
+
+          <View className="flex-row items-center justify-center pt-5 gap-2">
+            <Text className="text-sm font-pregular text-gray-100">
+              Already have an account?
+            </Text>
+            <Link
+              href="/login"
+              className="text-blue-600 font-pregular  text-sm"
+            >
+              Login
+            </Link>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-      <StatusBar style="light" />
-    </>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
